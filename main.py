c384d3053d88fda44d0c871e66943262b2c8ab25
Ujjwal <itsujji@gmail.com>
2022-03-22 18:57:00 +0530

    Template engine added

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from typing import Optional
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Request, Header
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="public", html=True), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/items")
-async def read_items(user_agent: Optional[str] = Header(None)):
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/user-agent")
+async def read_items(user_agent: str = Header(None)):
     return {"User-Agent": user_agent}
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("index.html", {"request": request, "id": id})
